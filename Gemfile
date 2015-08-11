02653fa9a613a95fd922d691816517625544670c
David Daniel <davedan7@gmail.com>
2015-08-11 09:29:04 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.3'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+# See https://github.com/rails/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'materialize-sass'
+
+# gem 'carrierwave', '~> 0.9'
+
+
+group :development, :test do
+  gem 'pry'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
+
+  gem 'web-console', '~> 2.0'
+end
+
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+# RSpec.describe Beer, type: :model do
+#   let(:beer) { Beer.create(name: "Titan",
+#                            state: true,
+#                            description: "aggressively hopped ipa",
+#                            price: 350) }
+
+#   let(:beer2) { Beer.create(name: "Titan2",
+#                            state: false,
+#                            description: "aggressively hopped ipa",
+#                            price: 350) }
+#   context "is valid" do
+#     it "is valid with valid attributes" do
+#       expect(beer).to be_valid
+#     end
+
+#     it "is valid with false state " do
+#       expect(beer2).to be_valid
+#     end
+#   end
+
+end
